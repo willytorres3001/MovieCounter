3544db3a30ec35743c1cc2aaec291ca6a2277067
willy <willy.torres@tecsup.edu.pe>
2026-04-16 13:57:21 -0500

    feat: Upgrade MiBoton style

--- app/src/main/java/com/example/moviecounter/ComponenteBoton.kt
+++ app/src/main/java/com/example/moviecounter/ComponenteBoton.kt
@@ -1,12 +1,24 @@
 package com.example.moviecounter
 
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun MiBoton() {
-    Button(onClick = {}) {
-        Text("Mi Primer Botón")
+    Button(
+        onClick = {},
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Blue
+        )
+    ) {
+        Text(
+            text = "Mi Primer Botón",
+            color = Color.White,
+            fontSize = 18.sp
+        )
     }
 }
