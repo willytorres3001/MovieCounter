f8f8c2ffbb8ecf146b4d6cdc5300bb06a95aeead
willy <willy.torres@tecsup.edu.pe>
2026-04-16 13:32:46 -0500

    feat: Add MiBoton component

---
+++ app/src/main/java/com/example/moviecounter/ComponenteBoton.kt
@@ -0,0 +1,12 @@
+package com.example.moviecounter
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MiBoton() {
+    Button(onClick = {}) {
+        Text("Mi Primer Botón")
+    }
+}
