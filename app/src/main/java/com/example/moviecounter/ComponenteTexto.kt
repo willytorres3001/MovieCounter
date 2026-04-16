c7dc22674cd0585c5d5eee87f32df20507f81e67
willy <willy.torres@tecsup.edu.pe>
2026-04-16 13:33:12 -0500

    feat: Add MiTexto component

---
+++ app/src/main/java/com/example/moviecounter/ComponenteTexto.kt
@@ -0,0 +1,13 @@
+package com.example.moviecounter
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun MiTexto() {
+    Text(
+        text = "Hola desde Compose!",
+        fontSize = 20.sp
+    )
+}
