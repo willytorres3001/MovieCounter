6a426d1ba290fa31df6b5e52a22bc5576d681aca
willy <willy.torres@tecsup.edu.pe>
2026-04-16 17:14:19 -0500

    feat: Cambio desarrollador A en MiBoton

--- app/src/main/java/com/example/moviecounter/ComponenteBoton.kt
+++ app/src/main/java/com/example/moviecounter/ComponenteBoton.kt
@@ -1,5 +1,5 @@
 package com.example.moviecounter
-
+// Cambio hecho por Desarrollador A
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
