bbcf4d4dc09aca688cf3eb43bb2a648d4bd8e41e
willy <willy.torres@tecsup.edu.pe>
2026-04-16 13:59:40 -0500

    feat: Upgrade MiTarjeta style

--- app/src/main/java/com/example/moviecounter/ComponenteTarjeta.kt
+++ app/src/main/java/com/example/moviecounter/ComponenteTarjeta.kt
@@ -2,17 +2,27 @@ package com.example.moviecounter
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun MiTarjeta() {
-    Card(modifier = Modifier.padding(16.dp)) {
+    Card(
+        modifier = Modifier.padding(16.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.LightGray
+        )
+    ) {
         Text(
             text = "Mi primera tarjeta",
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier.padding(16.dp),
+            fontSize = 20.sp,
+            color = Color.DarkGray
         )
     }
 }
